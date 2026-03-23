34a5f303bbd4540f54c2009c5a3467092d1a7b78
Douanla Gningaye Hermann Maslow <138356589+Hermann2710@users.noreply.github.com>
2026-03-23 18:16:09 +0100

    Mise à jour du flux d'authentification avec auth.js

--- auth.ts
+++ auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "@/lib/db";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(client),
-  providers: [],
+  providers: [Google, GitHub],
 });
